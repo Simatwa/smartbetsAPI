c16c87543b5f2cf343c28e1e29ff412680ba31f1
smartwa <smartwacaleb@gmail.com>
2024-04-10 17:49:37 +0300

    Minor updates

--- asgi.py
+++ asgi.py
@@ -2,10 +2,6 @@
 from sys import argv
 from os import environ
 
-argv.append(environ.get("USERNAME", "api"))
-
-argv += ["-u", environ.get("PASSWORD", "developer")]
-
 from smartbets_API.interface import app
 
 if __name__ == "__main__":
