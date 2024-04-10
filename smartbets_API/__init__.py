720738ae24c56d345dff5315fcc8d8208cc22fc6
smartwa <smartwacaleb@gmail.com>
2024-04-10 16:19:30 +0300

    Update dependencies

--- smartbets_API/__init__.py
+++ smartbets_API/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "1.2.0"
 __author__ = "Smartwa Caleb"
-from .bet_at_api_level import predictor as rest_api
+from .bet_at_rest_api_level import predictor as rest_api
 from .predictor import predictor
 
 __all__ = [
